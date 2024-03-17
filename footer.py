07c841121be4390df96b217dde8ef85990994f0c
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-03-17 01:50:50 +0100

    style: :art: Update style of the frontend

--- footer.py
+++ footer.py
@@ -1,14 +1,36 @@
 DESCRIPTION = """
 
-<h1> <center> 🤗 Hello. This App will help you do questions on youtube videos.</center> </h1>
+<h1> <center> With LangChain 🦜🔗 & Gradio, chat with a YouTube Video.</center> </h1>
 
 <h4>
-Follow these steps to use 😉:
+You have the following tabs:
 </h4>
 
 <ol>
-  <li>Set your OpenAI Key</li>
-  <li>Set your Youtube URL</li>
-  <li>Ask!</li>
+  <li> </h4>QnA</h4>: To perform a conversation</li>
+  <li> </h4>Summary - Paragraph</h4> : To view the summary as a whole detailed paragraph using Refine method</li>
+  <li> </h4>Summary - Key Points</h4> : To view the summary as main points using Map Reduce method</li>
 </ol> 
+"""
+
+loading_message = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading...</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h2>Loading... Please wait.</h2>
+</body>
+</html>
 """
